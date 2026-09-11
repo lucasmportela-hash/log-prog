6ec837779811f1ab9d80386e480c904dbb22a649
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-11 19:46:39 -0300

    modified:   Python/TresValoresNumericos/TresValoresNumericos.py

--- Python/TresValoresNumericos/TresValoresNumericos.py
+++ Python/TresValoresNumericos/TresValoresNumericos.py
@@ -2,13 +2,12 @@ a = float (input ("Informe o valor de a: "))
 b = float (input ("Informe o valor de b: "))
 c = float (input ("Informe o valor de c: "))
 
-if(a + b > c and a + c > b and b + c > a):
+if(a > 0 and b > 0 and c > 0):
     if (a == b and a == c):
         print("Equilátero")
     elif(a == b or a == c or b == c):
         print("Isósceles")
-else:
-    if(a > 0 and b > 0 and c > 0):
-        print("Escaleno")
     else:
+        print("Escaleno")
+else:
         print("Erro")
