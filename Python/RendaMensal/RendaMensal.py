9a32aa99adca25c58d0dfcb2b463d364eb7112a9
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-11 21:28:04 -0300

    modified:   Python/RendaMensal/RendaMensal.py

--- Python/RendaMensal/RendaMensal.py
+++ Python/RendaMensal/RendaMensal.py
@@ -1,6 +1,13 @@
 RendaMensal = float
 score = int 0 <= 1000 com restrição
 
-aprovado | score >= 700 and RendaMensal > 4000 and sem restricoes (bool)
-reaprovar | RendaMensal >= 2500 and sem restricoes and score >= 500 or RendaMensal > 6000
-recursar | else
+RendaMensal = float (input("Informe sua renda mensal: "))
+score = int (input("Informe seu score: "))
+Restricoes = bool (input("Possui restrições (True/False): "))
+
+if (score >= 700 and RendaMensal > 4000 and Restricoes = False)
+    print("APROVADO")
+elif((RendaMensal >= 2500 and Restricoes = "False" and score >= 500) or RendaMensal >6000):
+    print("APROVADO")
+else:
+    print("REPROVADO")
