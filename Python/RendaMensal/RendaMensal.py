d3c05c82db2713b2d855d13c199de12f517fdfb6
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-11 21:21:42 -0300

    modified:   Python/RendaMensal/RendaMensal.py

--- Python/RendaMensal/RendaMensal.py
+++ Python/RendaMensal/RendaMensal.py
@@ -1,1 +1,6 @@
-RendaMensal = 1.500
+RendaMensal = float
+score = int 0 <= 1000 com restrição
+
+aprovado | score >= 700 and RendaMensal > 4000 and sem restricoes (bool)
+reaprovar | RendaMensal >= 2500 and sem restricoes and score >= 500 or RendaMensal > 6000
+recursar | else
