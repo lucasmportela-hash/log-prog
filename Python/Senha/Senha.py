d64e622a773e99520388bdedbde05787f8f6ae25
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-19 12:21:26 -0300

    modified:   Python/Senha/Senha.py

--- Python/Senha/Senha.py
+++ Python/Senha/Senha.py
@@ -1,11 +1,21 @@
+import time
 senha = 0 
-tentativa = 4
+tentativa = 3
 while (senha < 10000 and senha < 1000):
     senha = int (input("Informe uma senha: "))
-while (tentativa != 1):
-    resposta = int (input("Digite a senha "))
+
+while (tentativa != 0):
+    resposta = int (input("Digite a senha: "))
     if (resposta == senha):
         print("Acesso concedido")
+        break   
     else:
-        print(f"Acesso negado: {tentativa} restante")
         tentativa = tentativa - 1
+        print(f"Acesso negado: {tentativa} restante")
+        if (tentativa == 0):
+            print("Conta bloqueada")
+print("Sistema inicializando em:")
+for i in range(5, 0, -1):
+    print(i)
+    time.sleep(1)
+print("Sistema inicializado :D")
