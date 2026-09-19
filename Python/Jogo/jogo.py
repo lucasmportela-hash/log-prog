9c75be128667a592ad3dba7720d210906a4f8255
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-18 21:18:27 -0300

    modified:   Python/Jogo/jogo.py

--- Python/Jogo/jogo.py
+++ Python/Jogo/jogo.py
@@ -1,34 +1,43 @@
-
-rodada = 0
+import random
+rodada = 1
 tentativa = 0 
 pontos = 100
-
-while (rodada != 3):
-    import random
+pontosTotais = 0 
+while (rodada != 4):
     num = random.randint(1, 50)
     while (tentativa != 5):
         print (num)
+        print (f"Rodada: {rodada}")
+        tentativa = tentativa + 1
+        print (f"Tentativa: {tentativa}")
         palpite = int (input("Adivinhe um número entre 1 e 50: "))
         if (palpite > num):
             print("Palpite maior que o número")
-            tentativa = tentativa + 1
-            pontos = pontos - 25
+            pontos = 0
         elif(palpite < num):
             print("Palpite menor que o número")
-            tentativa = tentativa + 1
-            pontos = pontos - 25
+            pontos = 0
         else:
             print("Palpite certo")
+            if (tentativa == 1):
+                pontos = 100
+            elif (tentativa == 2):
+                pontos = 75
+            elif (tentativa == 3):
+                pontos = 50
+            elif (tentativa == 4):
+                pontos = 25
+            elif (tentativa == 5):
+                pontos = 10
             tentativa = 5
+    tentativa = 0 
+    pontosTotais = pontosTotais + pontos
     rodada = rodada + 1
-print(pontos)
 
-#Encerrar a rodada caso o jogador acerte, somando
-#pontos: 100 pontos na 1a tentativa, 75 na 2a, 50 na 3a, 25
-#na 4a e 10 na 5a.
-#● Indicar fim de tentativas na rodada com 0 pontos ganhos
-#se esgotar as 5 chances.
+if (pontosTotais > 200):
+    print(f"Excelente pontuação de: {pontosTotais}")
+elif (pontosTotais >= 100):
+    print(f"Bom pontuação de: {pontosTotais}")
+elif (pontosTotais <100):
+    print("Tente novamente :( ")
 
-#■ No final das 3 rodadas, avalie a pontuação total com if/elif/else e
-#imprima a classificação: "Excelente" (acima de 200), "Bom"
-#(entre 100 e 200) ou "Tente Novamente" (abaixo de 100).
