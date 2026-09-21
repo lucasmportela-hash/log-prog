d68924eb570dce4ad440d74e6d14943e68411e54
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-21 20:33:59 -0300

    modified:   Python/Media5Alunos/Media5Alunos.py

--- Python/Media5Alunos/Media5Alunos.py
+++ Python/Media5Alunos/Media5Alunos.py
@@ -6,7 +6,7 @@ Nomes = []
 i = 0
 while i < 5:
     Nome = str(input(f"Digite o seu nome aluno {i + 1}: "))
-    Nota = float(input(f"Digite a sua média: "))
+    Nota = float(input("Digite a sua média: "))
     if (Nota >= 0 and Nota <= 10):
         Nomes.append(Nome)
         Notas.append(Nota)
