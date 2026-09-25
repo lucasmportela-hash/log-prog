452a6a9da456b541e01166c98e61f21d1deff79c
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-25 19:05:38 -0300

    modified:   Python/MaiorDeIdadeClasse/MaiorDeIdadeClasse.py

--- Python/MaiorDeIdadeClasse/MaiorDeIdadeClasse.py
+++ Python/MaiorDeIdadeClasse/MaiorDeIdadeClasse.py
@@ -2,16 +2,17 @@
 # altura (em centímetros). O programa dirá se a pessoa é maior ou menor de idade.
 class Dados:
     def __init__(self, Nome, Idade, Altura):
-        self.Nome = Nome
+        self.Nome= Nome
         self.Idade = Idade
         self.Altura = Altura
     def Verificar(self):
         if self.Idade >= 18:
-            return ("Maior de idade")
+            return ("maior de idade")
         else:
-            return ("Menor de idade")
-Nome = str (input("Digite seu nome: "))
-Idade = int (input("Digite sua idade: "))
-Altura = float(input("Digite sua altura: "))
-Pessoa = Dados(Nome, Idade, Altura)
-print(f"{Nome} você tem {Altura} e é {Pessoa.Verificar()}")
+            return ("menor de idade")
+for i in range(5):
+    Nome = str (input(f"Digite seu nome {i+1}: "))
+    Idade = int (input("Digite sua idade: "))
+    Altura = float(input("Digite sua altura: "))
+    Pessoa = Dados(Nome, Idade, Altura)
+    print(f"{Nome} você tem {Altura:.2f} de altura é {Pessoa.Verificar()}")
