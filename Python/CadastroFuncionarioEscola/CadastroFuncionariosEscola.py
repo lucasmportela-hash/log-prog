4b93331e04b3212434d35f5a306538090e845b06
lucasmportela-hash <lucas.m.portela@aluno.senai.br>
2026-09-25 19:48:12 -0300

    modified:   Python/CadastroFuncionarioEscola/CadastroFuncionariosEscola.py

--- Python/CadastroFuncionarioEscola/CadastroFuncionariosEscola.py
+++ Python/CadastroFuncionarioEscola/CadastroFuncionariosEscola.py
@@ -1,24 +1,29 @@
-# Construa uma página/programa cadastrando cinco funcionários de uma escola, sendo
-# que dois deles são professores. Todos os funcionários terão cadastrados seus nomes e
-# salários e, os professores, terão cadastrados a disciplina às quais lecionam.
+# Construa um programa cadastrando cinco funcionários de uma escola,
+# sendo que dois deles são professores.
+# Todos os funcionários terão cadastrados seus nomes e salários.
+# Os professores terão cadastrada também a disciplina que lecionam.
 class Funcionarios:
-    def __init__(self, Nome, Salario, Disciplina, Cargo):
-        self.Nome = Nome
-        self.Salario = Salario
-        self.Cargo = Cargo
-        if (Cargo == "professor"):
-            self.Disciplina = Disciplina
-for i in range(5):
-    Nome = str(input(f"Digite seu nome {i+1}: "))
-    Salario = float(input("Digite seu salario: "))
-    Cargo = str(input("Digite seu cargo: ").lower())
-    if (Cargo == "professor"):
-        Disciplina = str(input("Digite sua disciplina: "))
-    else:
-        Disciplina = ""
-    Funcionario = Funcionarios(Nome, Salario, Cargo, Disciplina)
-    if Salario >= 2000:
-        print(f"Salario Acima de 2000! {Nome}")
-    if (Disciplina == "Matemática"):
-       print(f"{Nome} Você leciona Matemática!") 
-    print(f"{Nome} você é {Cargo} e tem um salário de {Salario:.2f}")
+    def __init__(self, nome, salario, cargo, disciplina):
+        self.nome = nome
+        self.salario = salario
+        self.cargo = cargo
+        self.disciplina = disciplina
+    def mostrar_dados(self):
+        print(f"Nome: {self.nome}")
+        print(f"Cargo: {self.cargo}")
+        if self.disciplina == "Matemática":
+            print(f"{self.nome} leciona Matemática!")
+        if self.salario >= 2000:
+            print("Salário acima de 2000!")
+funcionarios = []
+for i in range(1):
+    nome = input("Digite seu nome: ")
+    salario = float(input("Digite seu salário: "))
+    cargo = input("Digite seu cargo: ").strip().lower()
+    disciplina = ""
+    if cargo == "professor":
+        disciplina = input("Digite sua disciplina: ").strip()
+    funcionario = Funcionarios(nome, salario, cargo, disciplina)
+    funcionarios.append(funcionario)
+for funcionario in funcionarios:
+    funcionario.mostrar_dados()
